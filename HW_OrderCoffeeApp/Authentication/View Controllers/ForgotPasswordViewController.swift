0a201c0b92009495c2cc8d2fddd8e90faac38bcf
a578ff6 <a578ff6@gmail.com>
2024-09-30 23:03:59 +0800

    Refactor authentication view controllers to use async/await and enhance login methods
    - Replaced completion callback with async/await in ForgotPasswordViewController, LoginViewController, and SignUpViewController.
    - Enhanced googleLoginButtonTapped and appleLoginButtonTapped in SignUpViewController.

--- HW_OrderCoffeeApp/Authentication/View Controllers/ForgotPasswordViewController.swift
+++ HW_OrderCoffeeApp/Authentication/View Controllers/ForgotPasswordViewController.swift
@@ -5,78 +5,9 @@
 //  Created by 曹家瑋 on 2023/12/14.
 //
 
-// MARK: - storyboard版本
-/*
-import UIKit
-
-/// 忘記密碼頁面
-class ForgotPasswordViewController: UIViewController {
-
-    
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var resetPasswordButton: UIButton!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        setUpElements()
-        setUpHideKeyboardOntap()
-    }
-    
-    
-    /// 設置 UI 元件的樣式
-    func setUpElements() {
-        emailTextField.styleTextField()
-        resetPasswordButton.styleFilledButton()
-    }
-    
-    
-    /// 處理重置密碼按鈕點擊
-    @IBAction func resetPasswordButtonTapped(_ sender: UIButton) {
-        // 確保電子郵件輸入不為空
-        guard let email = emailTextField.text, !email.isEmpty else {
-            AlertService.showAlert(withTitle: "錯誤", message: "請填寫您的信箱", inViewController: self)
-            return
-        }
-        
-        // 檢查電子郵件格式是否有效
-        if !FirebaseController.isEmailvalid(email) {
-            AlertService.showAlert(withTitle: "錯誤", message: "無效的電子郵件格式", inViewController: self)
-            return
-        }
-        
-        ActivityIndicatorManager.shared.startLoading(on: view, backgroundColor: UIColor.black.withAlphaComponent(0.5)) // 啟動活動指示器
-        // 使用 FirebaseController 發送密碼重置郵件
-        FirebaseController.shared.resetPassword(forEmail: email) { [weak self] result in
-            DispatchQueue.main.async {
-                
-                ActivityIndicatorManager.shared.stopLoading()            // 停止活動指示器
-                
-                switch result {
-                case .success():
-                    // 發送成功，顯示成功訊息
-                    print("Password reset email sent successfully")
-                    AlertService.showAlert(withTitle: "通知", message: "如果您的信箱已註冊，我們將發送密碼重置郵件給您。", inViewController: self!) {
-                        // 關閉模態視圖控制器
-                        self?.dismiss(animated: true, completion: nil)
-                    }
-                case .failure(let error):
-                    // 發送失敗，顯示錯誤信息
-                    print("Error sending password reset email: \(error.localizedDescription)")
-                    AlertService.showAlert(withTitle: "錯誤", message: error.localizedDescription, inViewController: self!)
-                }
-            }
-            
-        }
-        
-    }
-    
-}
-*/
-
 
 // MARK: - 整理
+/*
 import UIKit
 
 /// 忘記密碼頁面
@@ -149,6 +80,96 @@ class ForgotPasswordViewController: UIViewController {
                 }
             }
             
+        }
+    }
+    
+    /// 跳轉回到 LoginViewController
+    @objc private func loginButtonTapped() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /// 處理關閉按鈕點擊事件
+    @objc private func closeButtonTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+*/
+
+
+// MARK: - 整理（async/await）
+
+import UIKit
+
+/// 忘記密碼頁面
+class ForgotPasswordViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let forgotPasswordView = ForgotPasswordView()
+    
+
+    // MARK: - Lifecycle Methods
+    override func loadView() {
+        view = forgotPasswordView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setUpHideKeyboardOntap()
+        setupCloseButton()
+        setupActions()
+    }
+    
+    // MARK: - Setup Methods
+    
+    /// 設置導航欄的關閉按鈕
+    private func setupCloseButton() {
+        let closeButton = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(closeButtonTapped))
+        navigationItem.leftBarButtonItem = closeButton
+    }
+    
+    /// 設置 重置密碼按鈕 和 回到 Login 頁面 按鈕的動作
+    private func setupActions() {
+        forgotPasswordView.resetPasswordButton.addTarget(self, action: #selector(resetPasswordButtonTapped), for: .touchUpInside)
+        forgotPasswordView.loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+    }
+
+    // MARK: - Action Methods
+    
+    /// 處理重置密碼按鈕點擊
+    @objc func resetPasswordButtonTapped() {
+        Task {
+            // 確保電子郵件輸入不為空
+            guard let email = forgotPasswordView.emailTextField.text, !email.isEmpty else {
+                AlertService.showAlert(withTitle: "錯誤", message: "請填寫您的信箱", inViewController: self)
+                return
+            }
+            
+            // 檢查電子郵件格式是否有效
+            if !EmailSignInController.isEmailvalid(email) {
+                AlertService.showAlert(withTitle: "錯誤", message: "無效的電子郵件格式", inViewController: self)
+                return
+            }
+            
+            HUDManager.shared.showLoading(text: "Sending...")
+            do {
+                // 使用 EmailSignInController 發送密碼重置郵件
+                try await EmailSignInController.shared.resetPassword(forEmail: email)
+                
+                // 發送成功，顯示成功訊息
+                print("Password reset email sent successfully")
+                AlertService.showAlert(withTitle: "通知", message: "如果您的信箱已註冊，我們將發送密碼重置郵件給您。", inViewController: self) {
+                    // 關閉模態視圖控制器
+                    self.dismiss(animated: true, completion: nil)
+                }
+            } catch {
+                // 發送失敗，顯示錯誤信息
+                print("Error sending password reset email: \(error.localizedDescription)")
+                AlertService.showAlert(withTitle: "錯誤", message: error.localizedDescription, inViewController: self)
+            }
+            
+            HUDManager.shared.dismiss()
         }
     }
     
--- HW_OrderCoffeeApp/Authentication/View Controllers/SignUpViewController.swift
+++ HW_OrderCoffeeApp/Authentication/View Controllers/SignUpViewController.swift
@@ -19,6 +19,7 @@
 
 
 // MARK: - 程式碼版本（註冊須知）
+/*
 import UIKit
 
 /// 註冊頁面
@@ -168,94 +169,172 @@ class SignUpViewController: UIViewController {
      }
 
 }
+*/
 
 
+// MARK: - 程式碼版本（註冊須知）(async/await)
 
-
-
-
-
-
-// MARK: - storyboard
-
-/*
 import UIKit
 
 /// 註冊頁面
 class SignUpViewController: UIViewController {
-
     
-    @IBOutlet weak var fullNameTextField: UITextField!
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var signUpButton: UIButton!
+    // MARK: - Properties
+    private let signUpView = SignUpView()
     
+    /// 追蹤使用者是否點擊了「註冊須知連結」
+    private var didViewTerms = false
+    
+    
+    // MARK: - Lifecycle Methods
+    override func loadView() {
+        view = signUpView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setUpElements()
+        view.backgroundColor = .white
         setUpHideKeyboardOntap()
-
+        setupActions()
     }
     
     
-    /// 設置界面樣式
-    func setUpElements() {
-        fullNameTextField.styleTextField()
-        emailTextField.styleTextField()
-        passwordTextField.styleTextField()
-        signUpButton.styleFilledButton()
+    // MARK: - Private Methods
+    /// 設置 Action
+    private func setupActions() {
+        signUpView.signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+        signUpView.setPasswordOrConfirmPasswordTextFieldIcon(for: signUpView.passwordTextField, target: self, action: #selector(togglePasswordVisibility))
+        signUpView.setPasswordOrConfirmPasswordTextFieldIcon(for: signUpView.confirmPasswordTextField, target: self, action: #selector(toggleConfirmPasswordVisibility))
+        
+        signUpView.googleLoginButton.addTarget(self, action: #selector(googleLoginButtonTapped), for: .touchUpInside)
+        signUpView.appleLoginButton.addTarget(self, action: #selector(appleLoginButtonTapped), for: .touchUpInside)
+        
+        signUpView.termsButton.addTarget(self, action: #selector(termsButtonTapped), for: .touchUpInside)
+        signUpView.termsCheckBox.addTarget(self, action: #selector(termsCheckBoxTapped), for: .touchUpInside)
     }
     
-    
-    /// 處理註冊按鈕
-    @IBAction func signUpButtonTapped(_ sender: UIButton) {
-        
-        // 檢查 TextField 是否有輸入，並且不為空
-        guard let email = emailTextField.text, !email.isEmpty,
-              let password = passwordTextField.text, !password.isEmpty,
-              let fullName = fullNameTextField.text, !fullName.isEmpty else {
-            AlertService.showAlert(withTitle: "錯誤", message: "請填寫所有資料", inViewController: self)
-            return
-        }
-        
-        // 檢查電子郵件格式是否有效
-        if !FirebaseController.isEmailvalid(email) {
-            AlertService.showAlert(withTitle: "錯誤", message: "請輸入有效的電子郵件地址，例如：example@example.com", inViewController: self)
-            return
-        }
-        
-        // 檢查密碼是否符合規範
-        if !FirebaseController.isPasswordValid(password) {
-            AlertService.showAlert(withTitle: "錯誤", message: "密碼需至少包含8位字符，並包括至少一個小寫字母和一個特殊字符", inViewController: self)
-            return
-        }
-        
-        ActivityIndicatorManager.shared.startLoading(on: view, backgroundColor: UIColor.black.withAlphaComponent(0.5)) // 啟動活動指示器
-        
-        // 調用 FirebaseController 進行用戶註冊
-        FirebaseController.shared.registerUser(withEmail: email, password: password, fullName: fullName) { [weak self] result in
-            DispatchQueue.main.async {
-                ActivityIndicatorManager.shared.stopLoading()            // 停止活動指示器
-
-                switch result {
-                case .success(_):
-                    FirebaseController.shared.getCurrentUserDetails { userDetailsResult in
-                        switch userDetailsResult {
-                        case .success(let userDetails):
-                            NavigationHelper.navigateToMainTabBar(from: self!, with: userDetails)   // 使用 NavigationHelper 導入到mainTabBarController
-                        case .failure(let error):
-                            AlertService.showAlert(withTitle: "錯誤", message: error.localizedDescription, inViewController: self!)
-                        }
-                    }
-                case .failure(let error):
-                    AlertService.showAlert(withTitle: "錯誤", message: error.localizedDescription, inViewController: self!)
+    /// 註冊須知連結
+    @objc private func termsButtonTapped() {
+        if let url = URL(string: "https://www.starbucks.com.tw/stores/allevent/show.jspx?n=1016") {
+            UIApplication.shared.open(url) { [weak self] success in
+                if success {
+                    self?.didViewTerms = true                        // 確認使用者點擊了連結
+                    self?.signUpView.termsCheckBox.isEnabled = true   // 啟用「確認框」
                 }
             }
         }
-        
+    }
+    
+    /// 確認框點擊
+    @objc private func termsCheckBoxTapped() {
+        guard didViewTerms else {
+            signUpView.termsCheckBox.isSelected = false         // 使用者還沒點擊「註冊須知連結」，不允許勾選
+            return
+        }
+        signUpView.termsCheckBox.isSelected.toggle()    // 切換選中狀態
     }
 
+    // MARK: - 處理一般 Email 註冊
+
+    @objc private func signUpButtonTapped() {
+        Task {
+            // 確認使用者有點擊「確認框」
+            guard signUpView.termsCheckBox.isSelected else {
+                AlertService.showAlert(withTitle: "錯誤", message: "請同意並閱讀註冊須知", inViewController: self)
+                return
+            }
+            
+            // 檢查 TextField 是否有輸入，並且不為空
+            guard let email = signUpView.emailTextField.text, !email.isEmpty,
+                  let password = signUpView.passwordTextField.text, !password.isEmpty,
+                  let fullName = signUpView.fullNameTextField.text, !fullName.isEmpty,
+                  let confirmPassword = signUpView.confirmPasswordTextField.text, !confirmPassword.isEmpty else {
+                AlertService.showAlert(withTitle: "錯誤", message: "請填寫所有資料", inViewController: self)
+                return
+            }
+            
+            // 檢查電子郵件格式是否有效
+            if !EmailSignInController.isEmailvalid(email) {
+                AlertService.showAlert(withTitle: "錯誤", message: "請輸入有效的電子郵件地址，例如：example@example.com", inViewController: self)
+                return
+            }
+            
+            // 檢查密碼是否符合規範
+            if !EmailSignInController.isPasswordValid(password) {
+                AlertService.showAlert(withTitle: "錯誤", message: "密碼需至少包含8位字符，並包括至少一個小寫字母和一個特殊字符", inViewController: self)
+                return
+            }
+            
+            // 檢查兩次密碼是否相同
+            if password != confirmPassword {
+                AlertService.showAlert(withTitle: "錯誤", message: "兩次輸入的密碼不一致", inViewController: self)
+                return
+            }
+            
+            HUDManager.shared.showLoading(text: "Signing up...")
+            do {
+                /// 調用 `EmailSignInController` 進行用戶註冊
+                let authResult = try await EmailSignInController.shared.registerUser(withEmail: email, password: password, fullName: fullName)
+                /// 註冊成功後，獲取`用戶詳細資料`並導航至 `mainTabBarController`
+                let userDetails = try await FirebaseController.shared.getCurrentUserDetails()
+                NavigationHelper.navigateToMainTabBar(from: self, with: userDetails)
+            } catch {
+                AlertService.showAlert(withTitle: "錯誤", message: error.localizedDescription, inViewController: self)
+            }
+            HUDManager.shared.dismiss()
+        }
+    }
+    
+    // MARK: - 處理Google登入
+    
+    @objc private func googleLoginButtonTapped() {
+        Task {
+            HUDManager.shared.showLoading(text: "Logging in...")
+            do {
+                let authResult = try await GoogleSignInController.shared.signInWithGoogle(presentingViewController: self)
+                let userDetails = try await FirebaseController.shared.getCurrentUserDetails()
+                NavigationHelper.navigateToMainTabBar(from: self, with: userDetails)
+            } catch {
+                AlertService.showAlert(withTitle: "錯誤", message: error.localizedDescription, inViewController: self)
+            }
+            HUDManager.shared.dismiss()
+        }
+    }
+        
+    // MARK: - 處理Apple登入
+    
+    @objc private func appleLoginButtonTapped() {
+        Task {
+            HUDManager.shared.showLoading(text: "Logging in...")
+            do {
+                let _ = try await AppleSignInController.shared.signInWithApple(presentingViewController: self)
+                let userDetails = try await FirebaseController.shared.getCurrentUserDetails()
+                NavigationHelper.navigateToMainTabBar(from: self, with: userDetails)
+            } catch {
+                AlertService.showAlert(withTitle: "錯誤", message: error.localizedDescription, inViewController: self)
+            }
+            HUDManager.shared.dismiss()
+        }
+    }
+    
+    // MARK: - Private Methods
+
+    /// 切換密碼顯示狀態
+     @objc private func togglePasswordVisibility() {
+         signUpView.passwordTextField.isSecureTextEntry.toggle()
+         let eyeImageName = signUpView.passwordTextField.isSecureTextEntry ? "eye" : "eye.slash"
+         if let eyeButton = signUpView.passwordTextField.rightView as? UIButton {
+             eyeButton.setImage(UIImage(systemName: eyeImageName), for: .normal)
+         }
+     }
+     
+     /// 切換確認密碼顯示狀態
+     @objc private func toggleConfirmPasswordVisibility() {
+         signUpView.confirmPasswordTextField.isSecureTextEntry.toggle()
+         let eyeImageName = signUpView.confirmPasswordTextField.isSecureTextEntry ? "eye" : "eye.slash"
+         if let eyeButton = signUpView.confirmPasswordTextField.rightView as? UIButton {
+             eyeButton.setImage(UIImage(systemName: eyeImageName), for: .normal)
+         }
+     }
+
 }
-*/
