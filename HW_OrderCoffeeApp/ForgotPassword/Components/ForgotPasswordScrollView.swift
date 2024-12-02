2de20adca188c04e7542377700bc0c28a7dac95a
a578ff6 <a578ff6@gmail.com>
2024-12-03 01:33:11 +0800

    Add ForgotPassword custom components
    * Added ForgotPasswordAttributedButton.swift:
    - A custom button supporting multi-styled text, typically used for links or prompts.
    
    * Added ForgotPasswordFilledButton.swift:
    - Custom filled button for primary actions like 'Reset Password.'
    
    * Added ForgotPasswordLabel.swift:
    - A standardized UILabel for ForgotPasswordView's text elements.
    
    * Added ForgotPasswordScrollView.swift:
    - A custom ScrollView for structured layouts in ForgotPassword views.
    
    * Added ForgotPasswordStackView.swift:
    - A custom StackView with unified configurations for ForgotPassword layouts.
    
    * Added ForgotPasswordTextField.swift:
    - Custom text field inheriting from BottomLineTextField, specifically for email input in ForgotPasswordView.

---
+++ HW_OrderCoffeeApp/ForgotPassword/Components/ForgotPasswordScrollView.swift
@@ -0,0 +1,40 @@
+//
+//  ForgotPasswordScrollView.swift
+//  HW_OrderCoffeeApp
+//
+//  Created by 曹家瑋 on 2024/12/2.
+//
+
+import UIKit
+
+/// 自訂的 ScrollView 類別，用於 ForgotPasswordView 相關視圖中，提供標準化設置與佈局
+class ForgotPasswordScrollView: UIScrollView {
+    
+    // MARK: - Initializers
+    
+    /// 初始化 ForgotPasswordScrollView，並配置標準設置與佈局
+    init() {
+        super.init(frame: .zero)
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Public Methods
+    
+    /// 設置 ScrollView 的約束條件
+    /// - Parameter parentView: 父視圖，ScrollView 將設置其在此視圖中的約束
+    func setupConstraints(in parentView: UIView) {
+        parentView.addSubview(self)
+        NSLayoutConstraint.activate([
+            self.topAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.topAnchor),
+            self.leadingAnchor.constraint(equalTo: parentView.leadingAnchor),
+            self.trailingAnchor.constraint(equalTo: parentView.trailingAnchor),
+            self.bottomAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
+    }
+
+}
